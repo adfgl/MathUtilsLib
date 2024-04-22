ba140a89755360cb4cdf3f4086886f2f596ad38d
adfgl <69821550+adfgl@users.noreply.github.com>
2024-04-22 11:37:06 +0300

    add Vec2 structure

--- MathUtils/LinearAlgebraLib/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LinearAlgebraLib
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ MathUtils/LinearAlgebraLib/Vec2.cs
@@ -0,0 +1,16 @@
+﻿namespace LinearAlgebraLib
+{
+    public readonly struct Vec2
+    {
+        public readonly double x, y, w;
+        public readonly bool normalized;
+
+        public Vec2(double x, double y, double w, bool normalized)
+        {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.normalized = normalized;
+        }
+    }
+}
