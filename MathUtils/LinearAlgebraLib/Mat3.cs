6c66d3ef562f2f3bf61161f86def11880a46ecdb
adfgl <69821550+adfgl@users.noreply.github.com>
2024-04-22 13:41:43 +0300

    enable by row, col access

--- MathUtils/LinearAlgebraLib/Mat3.cs
+++ MathUtils/LinearAlgebraLib/Mat3.cs
@@ -13,5 +13,35 @@
             this.m31 = m31; this.m32 = m32; this.m33 = m33;
         }
 
+        public double Get(int row, int col)
+        {
+            return row switch
+            {
+                0 => col switch
+                {
+                    0 => m11,
+                    1 => m12,
+                    2 => m13,
+                    _ => throw new IndexOutOfRangeException($"Invalid column index {col}. Valid column indices are 0, 1, and 2."),
+                },
+                1 => col switch
+                {
+                    0 => m21,
+                    1 => m22,
+                    2 => m23,
+                    _ => throw new IndexOutOfRangeException($"Invalid column index {col}. Valid column indices are 0, 1, and 2."),
+                },
+                2 => col switch
+                {
+                    0 => m31,
+                    1 => m32,
+                    2 => m33,
+                    _ => throw new IndexOutOfRangeException($"Invalid column index {col}. Valid column indices are 0, 1, and 2."),
+                },
+                _ => throw new IndexOutOfRangeException($"Invalid row index {row}. Valid row indices are 0, 1, and 2."),
+            };
+        }
+
+
     }
 }
---
+++ MathUtils/LinearAlgebraTests/Mat3Tests.cs
@@ -0,0 +1,34 @@
+﻿using LinearAlgebraLib;
+
+namespace LinearAlgebraTests
+{
+    public class Mat3Tests
+    {
+        [Fact]
+        public void GetThrowsIndexOutOfRangeException()
+        {
+            var m = new Mat3(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            Assert.Throws<IndexOutOfRangeException>(() => m.Get(3, 0));
+            Assert.Throws<IndexOutOfRangeException>(() => m.Get(0, 3));
+            Assert.Throws<IndexOutOfRangeException>(() => m.Get(-1, 0));
+            Assert.Throws<IndexOutOfRangeException>(() => m.Get(0, -1));
+        }
+
+        [Fact]
+        public void GetReturnsCorrectCellValue()
+        {
+            Mat3 m = new Mat3(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            Assert.Equal(m.m11, m.Get(0, 0));
+            Assert.Equal(m.m12, m.Get(0, 1));
+            Assert.Equal(m.m13, m.Get(0, 2));
+
+            Assert.Equal(m.m21, m.Get(1, 0));
+            Assert.Equal(m.m22, m.Get(1, 1));
+            Assert.Equal(m.m23, m.Get(1, 2));
+
+            Assert.Equal(m.m31, m.Get(2, 0));
+            Assert.Equal(m.m32, m.Get(2, 1));
+            Assert.Equal(m.m33, m.Get(2, 2));
+        }
+    }
+}
