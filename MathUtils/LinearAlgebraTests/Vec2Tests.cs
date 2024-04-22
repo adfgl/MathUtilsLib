b3d9ff6a3db4e766dc6cd135465a45b43b983045
adfgl <69821550+adfgl@users.noreply.github.com>
2024-04-22 11:52:47 +0300

    add Deconstruct test

--- MathUtils/LinearAlgebraTests/Vec2Tests.cs
+++ MathUtils/LinearAlgebraTests/Vec2Tests.cs
@@ -1,11 +1,21 @@
+using LinearAlgebraLib;
+
 namespace LinearAlgebraTests
 {
     public class Vec2Tests
     {
         [Fact]
-        public void Test1()
+        public void DeconstructorWorksCorrectly()
         {
+            // Arrange
+            Vec2 v = new Vec2(12, 34, -2);
 
+            // Act
+            v.Deconstruct(out double actualX, out double actualY);
+
+            // Assert
+            Assert.Equal(v.x, actualX);
+            Assert.Equal(v.y, actualY);
         }
     }
 }
