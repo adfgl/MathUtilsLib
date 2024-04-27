afcd60778f5ba53fdb2193f7908398125944ac14
adfgl <69821550+adfgl@users.noreply.github.com>
2024-04-27 11:06:08 +0300

    enable direct operations

--- MathUtils/LinearAlgebraLib/Trans2.cs
+++ MathUtils/LinearAlgebraLib/Trans2.cs
@@ -2,7 +2,9 @@
 {
     public class Trans2
     {
-        bool s_inverseIsValid = false;
+        bool s_backwardIsValid = false;
+        bool s_forwardIsValid = false;
+        Stack<Mat3> s_matrices = new Stack<Mat3>();
 
         Mat3 _source;
         Mat3 _inverse;
@@ -13,7 +15,6 @@
             Reset();
         }
 
-        public Mat3 Source => _source;
         public bool IsDirty => _isDirty;
 
         public void Reset()
@@ -21,46 +22,46 @@
             _source = Mat3.Identity;
             _inverse = Mat3.Identity;
             _isDirty = false;
+            s_matrices.Clear();
         }
 
         void OnSourceMatrixChanged()
         {
             _isDirty = true;
+            s_backwardIsValid = false;
+            s_forwardIsValid = false;
+        }
+
+        void UpdateForward()
+        {
+            if (s_forwardIsValid) return;
+            while (s_matrices.Count > 0)
+            {
+                _source *= s_matrices.Pop();
+            }
+            s_forwardIsValid = true;
+        }
+
+        void UpdateBackward()
+        {
+            if (s_backwardIsValid) return;
+            if (false == _source.Inverse(out _inverse))
+            {
+                throw new DivideByZeroException("Can't invert matrix. Determinant is zero.");
+            }
+            s_backwardIsValid = true;
         }
 
         public Trans2 Translate(double x, double y)
         {
-            _source = Mat3.Multiply(_source, Mat3.Translation(x, y));
-            OnSourceMatrixChanged();
-            return this;
-        }
-
-        public Trans2 Scale(double scaleX, double scaleY)
-        {
-            _source = Mat3.Multiply(_source, Mat3.Scale(scaleX, scaleY));
-            OnSourceMatrixChanged();
-            return this;
-        }
-        public Trans2 ScaleAt(double x, double y, double scaleX, double scaleY)
-        {
-            _source = Mat3.Multiply(_source, Mat3.Translation(x, y));
-            _source = Mat3.Multiply(_source, Mat3.Scale(scaleX, scaleY));
-            _source = Mat3.Multiply(_source, Mat3.Translation(-x, -y));
+            s_matrices.Push(Mat3.Translation(x, y));
             OnSourceMatrixChanged();
             return this;
         }
 
         public Trans2 Rotate(double rad)
         {
-            _source = Mat3.Multiply(_source, Mat3.Rotation(rad));
-            OnSourceMatrixChanged();
-            return this;
-        }
-        public Trans2 RotateAt(double x, double y, double rad)
-        {
-            _source = Mat3.Multiply(_source, Mat3.Translation(x, y));
-            _source = Mat3.Multiply(_source, Mat3.Rotation(rad));
-            _source = Mat3.Multiply(_source, Mat3.Translation(-x, -y));
+            s_matrices.Push(Mat3.Rotation(rad));
             OnSourceMatrixChanged();
             return this;
         }
@@ -68,21 +69,14 @@
         public Vec2 Forward(Vec2 vec)
         {
             if (false == _isDirty) return vec;
+            UpdateForward();
             return _source * vec;
         }
 
         public Vec2 Backward(Vec2 vec)
         {
             if (false == _isDirty) return vec;
-
-            if (false == s_inverseIsValid)
-            {
-                if (false == _source.Inverse(out _inverse))
-                {
-                    throw new DivideByZeroException("Can't invert matrix. Determinant is zero.");
-                }
-                s_inverseIsValid = true;
-            }
+            UpdateBackward();
             return _inverse * vec;
         }
     }
--- MathUtils/MathUtilsConsole/Program.cs
+++ MathUtils/MathUtilsConsole/Program.cs
@@ -9,10 +9,18 @@ namespace MathUtilsConsole
         static void Main(string[] args)
         {
             Vec2[] vertices = [new Vec2(95.831, 48.003), new Vec2(170.185, 165.529)];
-
             foreach (var item in vertices)
             {
                 Console.WriteLine(item);
+            }
+
+            Trans2 trans2 = new Trans2().Translate(-136.94, -98.076).Rotate(Math.PI / 4);
+
+            Console.WriteLine();
+
+            foreach (var item in vertices)
+            {
+                Console.WriteLine(trans2.Forward(item));
             }
         }
     }
