5941c4a8bfc72eb61c7f8ebbb8a34ea22fe60ce2
adfgl <69821550+adfgl@users.noreply.github.com>
2024-04-27 13:35:07 +0300

    add plane

---
+++ MathUtils/GeometryLib/Plane.cs
@@ -0,0 +1,42 @@
+﻿using LinearAlgebraLib;
+
+namespace GeometryLib
+{
+    public readonly struct Plane
+    {
+        public readonly Vec3 normal;
+        public readonly double distanceToOrigin;
+
+        public Plane(Vec3 normal, double distanceToOrigin)
+        {
+            this.normal = normal.Normalize();
+            this.distanceToOrigin = distanceToOrigin;
+        }
+
+        public Plane(Vec3 normal, Vec3 point)
+        {
+            this.normal = normal.Normalize();
+            this.distanceToOrigin = normal.Dot(point);
+        }
+
+        public Plane(Vec3 a, Vec3 b, Vec3 c)
+        {
+            this.normal = (b - a).Cross(c - a).Normalize();
+            this.distanceToOrigin = normal.Dot(a);
+        }
+
+        /// <summary>
+        /// Calculates the signed distance from a specified point to the plane.
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns>The distance is <c>positive</c> if the point is in the direction of the plane's normal, <c>negative</c> otherwise.</returns>
+        public double SignedDistanceTo(Vec3 point)
+        {
+            return normal.Dot(point) - distanceToOrigin;
+        }
+
+        public bool IsValid() => normal.SquareLength() > 0.0;
+
+        public Plane Flip() => new Plane(-normal, -distanceToOrigin);
+    }
+}
