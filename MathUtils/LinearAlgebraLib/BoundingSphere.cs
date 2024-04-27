71a3bd29b5c6f6c35aa55ed01b344160c072ffc3
adfgl <69821550+adfgl@users.noreply.github.com>
2024-04-27 11:53:11 +0300

    add constructor

---
+++ MathUtils/LinearAlgebraLib/BoundingSphere.cs
@@ -0,0 +1,15 @@
+﻿namespace LinearAlgebraLib
+{
+    public readonly struct BoundingSphere
+    {
+        public readonly double cx, cy;
+        public readonly double radius;
+
+        public BoundingSphere(double cx, double cy, double radius)
+        {
+            this.cx = cx;
+            this.cy = cy;
+            this.radius = radius;
+        }
+    }
+}
