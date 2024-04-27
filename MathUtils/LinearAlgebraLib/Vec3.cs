4f054c0b094ee4f3625c8cd4442546c64c1d6427
adfgl <69821550+adfgl@users.noreply.github.com>
2024-04-27 08:47:00 +0300

    add constructor & deconstructor

---
+++ MathUtils/LinearAlgebraLib/Vec3.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace LinearAlgebraLib
+{
+    [DebuggerDisplay("{x}, {y}, {z}")]
+    public readonly struct Vec3
+    {
+        public readonly double x, y, z, w;
+        public readonly bool normalized;
+
+        public Vec3(double x, double y, double z, double w = 1, bool normalized = false)
+        {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.z = z;
+            this.normalized = normalized;
+        }
+
+        public void Deconstruct(out double x, out double y, out double z)
+        {
+            x = this.x;
+            y = this.y;
+            z = this.z;
+        }
+    }
+}
---
+++ MathUtils/LinearAlgebraTests/Vec3Tests.cs
@@ -0,0 +1,22 @@
+﻿using LinearAlgebraLib;
+
+namespace LinearAlgebraTests
+{
+    public class Vec3Tests
+    {
+        [Fact]
+        public void DeconstructWorksCorrectly()
+        {
+            // Arrange
+            Vec3 v = new Vec3(12, 34, -2);
+
+            // Act
+            v.Deconstruct(out double actualX, out double actualY, out double actualZ);
+
+            // Assert
+            Assert.Equal(v.x, actualX);
+            Assert.Equal(v.y, actualY);
+            Assert.Equal(v.z, actualZ);
+        }
+    }
+}
