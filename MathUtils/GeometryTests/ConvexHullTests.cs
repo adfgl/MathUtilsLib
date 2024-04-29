0eaad09fa73be0c95cfaa8084e90d39f769ba082
adfgl <69821550+adfgl@users.noreply.github.com>
2024-04-29 14:17:56 +0300

    finish tests for GetValidFace

--- MathUtils/GeometryTests/ConvexHullTests.cs
+++ MathUtils/GeometryTests/ConvexHullTests.cs
@@ -1,5 +1,6 @@
 ﻿using GeometryLib;
 using LinearAlgebraLib;
+using static GeometryLib.ConvexHull;
 
 namespace GeometryTests
 {
@@ -21,6 +22,46 @@ namespace GeometryTests
 
             // Assert
             Assert.Throws<InvalidOperationException>(act);
+        }
+
+        [Fact]
+        public void GetValidFace_FaceIsInvertedCorrectlyWhenReferencePointIsInFrontOfFace()
+        {
+            // Arrange
+            Vec3[] points =
+            [
+                new Vec3(0, 100, 0),
+                new Vec3(-100, -100, 0),
+                new Vec3(+100, -100, 0),
+            ];
+            Vec3 centroid = new Vec3(0, 0, 100);
+            Face expected = new Face(2, 1, 0, new Plane(new Vec3(0, 0, -1), 0));
+
+            // Act
+            Face actual = ConvexHull.GetValidFace(points, centroid, 0, 1, 2, 0);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetValidFace_FaceIsNotInvertedWhenReferencePointIsBehindFace()
+        {
+            // Arrange
+            Vec3[] points =
+            [
+                new Vec3(+100, -100, 0),
+                new Vec3(-100, -100, 0),
+                new Vec3(0, 100, 0),
+            ];
+            Vec3 centroid = new Vec3(0, 0, 100);
+            Face expected = new Face(0, 1, 2, new Plane(new Vec3(0, 0, -1), 0));
+
+            // Act
+            Face actual = ConvexHull.GetValidFace(points, centroid, 0, 1, 2, 0);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
