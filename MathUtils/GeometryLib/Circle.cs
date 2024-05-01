3bdf1ee1f61dab41c95b6acb35edea1484dce888
adfgl <69821550+adfgl@users.noreply.github.com>
2024-05-01 12:55:12 +0300

    add new circle constructor

--- MathUtils/GeometryLib/Circle.cs
+++ MathUtils/GeometryLib/Circle.cs
@@ -14,6 +14,35 @@ namespace GeometryLib
             this.radius = radius;
         }
 
+        public Circle(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            // https://stackoverflow.com/questions/62488827/solving-equation-to-find-center-point-of-circle-from-3-points
+            var x12 = x1 - x2;
+            var x13 = x1 - x3;
+
+            var y12 = y1 - y2;
+            var y13 = y1 - y3;
+
+            var y31 = y3 - y1;
+            var y21 = y2 - y1;
+
+            var x31 = x3 - x1;
+            var x21 = x2 - x1;
+
+            var sx13 = x1 * x1 - x3 * x3;
+            var sy13 = y1 * y1 - y3 * y3;
+            var sx21 = x2 * x2 - x1 * x1;
+            var sy21 = y2 * y2 - y1 * y1;
+
+            var f = (sx13 * x12 + sy13 * x12 + sx21 * x13 + sy21 * x13) / (2 * (y31 * x12 - y21 * x13));
+            var g = (sx13 * y12 + sy13 * y12 + sx21 * y13 + sy21 * y13) / (2 * (x31 * y12 - x21 * y13));
+            var c = -(x1 * x1) - y1 * y1 - 2 * g * x1 - 2 * f * y1;
+
+            this.cx = -g;
+            this.cy = -f;
+            this.radius = Math.Sqrt(cx * cx + cy * cy - c);
+        }
+
         public void Deconstruct(out Vec2 center, out double radius)
         {
             center = new Vec2(cx, cy);
