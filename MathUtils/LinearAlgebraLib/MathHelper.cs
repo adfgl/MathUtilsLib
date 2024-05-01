4563bf5848505386b07d33dc88e5466938e6d968
adfgl <69821550+adfgl@users.noreply.github.com>
2024-05-01 12:39:53 +0300

    add Resultant angle

--- MathUtils/LinearAlgebraLib/MathHelper.cs
+++ MathUtils/LinearAlgebraLib/MathHelper.cs
@@ -2,6 +2,21 @@
 {
     public static class MathHelper
     {
+        public const double PI = Math.PI;
+        public const double HALF_PI = 0.5 * PI;
+        public const double ONE_AND_HALF_PI = 1.5 * PI;
+        public const double TWO_PI = 2 * PI;
+
+        public const double DEG2RAD = PI / 180;
+        public const double RAD2DEG = 180 / PI;
+
+        public const double INFINITY = 99999;
+
+        public static bool IsInfinity(double value)
+        {
+            return Math.Abs(value) >= INFINITY;
+        }
+
         public static bool IsZero(double number, double tolerance)
         {
 #if DEBUG
@@ -18,6 +33,45 @@
         public static bool AreEqual(double a, double b, double tolerance)
         {
             return IsZero(a - b, tolerance);
+        }
+
+        public static double ResultantAngle(double x, double y)
+        {
+            if (y == 0)
+            {
+                if (x > 0) return 0;
+                return PI;
+            }
+
+            if (x == 0)
+            {
+                if (y > 0) return HALF_PI;
+                return ONE_AND_HALF_PI;
+            }
+
+            double atan = Math.Atan(Math.Abs(y / x));
+            if (x > 0)
+            {
+                if (y > 0) return atan;
+                if (y < 0) return TWO_PI - atan;
+            }
+            else
+            {
+                if (y > 0) return PI - atan;
+                if (y < 0) return PI + atan;
+            }
+
+            if (y > 0)
+            {
+                if (x > 0) return atan;
+                if (x < 0) return PI - atan;
+            }
+            else
+            {
+                if (x > 0) return TWO_PI - atan;
+                if (x < 0) return PI + atan;
+            }
+            return 0;
         }
     }
 }
--- MathUtils/LinearAlgebraTests/MathHelperTests.cs
+++ MathUtils/LinearAlgebraTests/MathHelperTests.cs
@@ -37,5 +37,23 @@ namespace LinearAlgebraTests
             bool result = MathHelper.AreEqual(a, b, tolerance);
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData(+1, +0, 0)] // 0, 360
+        [InlineData(+1, +1, 0.785398163)] // 45
+        [InlineData(+0, +1, 1.570796327)] // 90
+        [InlineData(-1, +1, 2.356194490)] // 135
+        [InlineData(-1, +0, 3.141592654)] // 180
+        [InlineData(-1, -1, 3.926990818)] // 225
+        [InlineData(+0, -1, 4.712388981)] // 270
+        [InlineData(+1, -1, 5.497787144)] // 315
+        public void ResultantAngle_WhenCalled_ReturnsCorrectValue(double x, double y, double expected)
+        {
+            // Act
+            double result = MathHelper.ResultantAngle(x, y);
+
+            // Assert
+            Assert.Equal(expected, result, 4);
+        }
     }
 }
